84e2ea27f42a1e75aa5085dafe73b3fba0ae35a2
nwoo12 <39656778+nwoo12@users.noreply.github.com>
2018-06-02 12:49:07 -0700

    Create changePageYoutube.js

---
+++ changePageYoutube.js
@@ -0,0 +1,4 @@
+var body = document.querySelector("body");
+body.innerHTML = '<video width="100%" controls autoplay><source src="https://www.w3schools.com/Html/mov_bbb.mp4" type="video/mp4"><source src="https://www.w3schools.com/Html/mov_bbb.ogg" type="video/ogg">Your browser does not support HTML5 video.</video>';
+
+//http://forgifs.com/gallery/d/247378-2/Kim-Jong-Un-North-Korea-tag-teamed.gif
